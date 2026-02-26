543d5a23b04047c41f9baa49ea5ead8c4b7e7027
yyuh <dustinohlemann@gmail.com>
2026-02-26 21:22:09 +0100

    nvim config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,11 +1,25 @@
 return {
-  {
-    "saghen/blink.cmp",
-    opts = {
-      keymap = {
-        ["<Tab>"] = { "accept", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<CR>"] = { "accept", "fallback" },
+  "saghen/blink.cmp",
+  version = "*",
+  opts = {
+    keymap = {
+      preset = "none",
+      ["<Tab>"]    = { "accept", "fallback" },
+      ["<C-e>"]   = { "cancel" },
+      ["<C-d>"]   = { "scroll_documentation_down" },
+      ["<C-u>"]   = { "scroll_documentation_up" },
+    },
+    appearance = {
+      use_nvim_cmp_as_default = false,
+      nerd_font_variant = "mono",
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 0,
       },
     },
   },
